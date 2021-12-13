901e8aa2df14ac694b9eb7a9da6fee9357861837
Anudeep Gunukula <anudeepgunukula@gmail.com>
2021-12-13 08:37:08 +0000

    commit after crud

--- blog/main.py
+++ blog/main.py
@@ -43,14 +43,21 @@ def showblog(id, response: Response, db: Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id ==
-                                 id).delete(synchronize_session=False)
+    db_blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not db_blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'blog {id} not found, create it first')
+    db_blog.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, blog: schemas.Blog, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update(blog.dict())
+    db_blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not db_blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f'blog {id} not found, create it first')
+    db_blog.update(blog.dict())
     db.commit()
     return blog
